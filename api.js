ed1968904ee85d2760be5ac18263f2039e5ab831
levshkatov <l.shkatov@gmail.com>
2019-09-19 23:26:32 +0700

    fix

--- api.js
+++ api.js
@@ -4,12 +4,12 @@ const router = express.Router();
 
 const types = {
   "skrill-dollar-gaming": "skrill_dollar_gaming",
-  "skrill-euro-gaming": "skrill_euro_gaming"
-  "neteller-dollar-gaming": "neteller_dollar_gaming"
-  "neteller-euro-gaming": "neteller_euro_gaming"
-  "skrill-dollar-common": "skrill_dollar_common"
-  "skrill-euro-common": "skrill_euro_common"
-  "neteller-dollar-common": "neteller_dollar_common"
+  "skrill-euro-gaming": "skrill_euro_gaming",
+  "neteller-dollar-gaming": "neteller_dollar_gaming",
+  "neteller-euro-gaming": "neteller_euro_gaming",
+  "skrill-dollar-common": "skrill_dollar_common",
+  "skrill-euro-common": "skrill_euro_common",
+  "neteller-dollar-common": "neteller_dollar_common",
   "neteller-euro-common": "neteller_euro_common",
 };
 
